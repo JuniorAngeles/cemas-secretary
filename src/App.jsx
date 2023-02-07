4027237338fb0a338952f583067b4786351bc557
JuniorAngeles <yuniorangeles06@gmail.com>
2023-02-07 17:46:43 -0400

    Add Heder in the proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./services/firebase.js";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import Buscador from "./components/Buscador";
-
 import Register from "./components/Registertudent";
 import ViewStundents from "./components/ViewStuden.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
       <HashRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Navigate to={"/Students"} />} />
           <Route path="/Students" element={<ViewStundents />} />
