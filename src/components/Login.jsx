dcb09f1d2644f7d36bc50dd27dffd8c493dd5cf8
JuniorAngeles <yuniorangeles06@gmail.com>
2023-03-25 23:42:53 -0400

    Login google

---
+++ src/components/Login.jsx
@@ -0,0 +1,50 @@
+import { loginWithGoogle } from "../services/firebase";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Button, Box } from "@mui/material";
+
+export function Login() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = () => {
+    loginWithGoogle()
+      .then((user) => {
+        const { uid, displayName, photoURL } = user;
+        setUser({ uid, displayName, photoURL });
+        localStorage.setItem("uid", uid); // Guardar el uid en el localStorage
+        navigate("/Students");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // useEffect(() => {
+  const uid = localStorage.getItem("uid"); // Obtener el uid del localStorage
+  if (uid) {
+    navigate("/Students"); // Si el uid está en el localStorage, redirigir al usuario a la ruta "/Students"
+  }
+  // }, [navigate]);
+
+  return (
+    <Box
+      mt={30}
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Button
+        onClick={handleGoogleLogin}
+        variant="contained"
+        disableElevation
+        color="error"
+        sx={{ backgroundColor: "#ff0000", fontWeight: "bold", height: "60px" }}
+      >
+        Login with Google
+      </Button>
+    </Box>
+  );
+}
