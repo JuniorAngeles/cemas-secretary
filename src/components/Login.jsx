88eea708e231f9389e16099d2f951e7574f05baa
JuniorAngeles <yuniorangeles06@gmail.com>
2023-03-27 22:31:08 -0400

    Style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,39 @@
-import { loginWithGoogle } from "../services/firebase";
+import { loginWithGoogle, loginWithGihub } from "../services/firebase";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Button, Box } from "@mui/material";
+import { Button, Stack, Typography, Grid, Paper } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { useSpring, animated } from "@react-spring/web";
+
 export function Login() {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
+  // Animation In stack for login
+  const fadeIn = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 1000 },
+  });
+
+  // login with google
   const handleGoogleLogin = () => {
     loginWithGoogle()
+      .then((user) => {
+        const { uid, displayName, photoURL } = user;
+        setUser({ uid, displayName, photoURL });
+        localStorage.setItem("uid", uid); // Guardar el uid en el localStorage
+        navigate("/Students");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // login with GitHub
+  const handleGitHubLogin = () => {
+    loginWithGihub()
       .then((user) => {
         const { uid, displayName, photoURL } = user;
         setUser({ uid, displayName, photoURL });
@@ -28,24 +53,73 @@ export function Login() {
   }, [navigate]);
 
   return (
-    <Box
-      mt={30}
+    <Grid
+      mt={20}
       sx={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
       }}
     >
-      <Button
-        onClick={handleGoogleLogin}
-        startIcon={<GoogleIcon />}
-        variant="contained"
-        disableElevation
-        color="error"
-        sx={{ backgroundColor: "#ff0000", fontWeight: "bold", height: "60px" }}
-      >
-        Login with Google
-      </Button>
-    </Box>
+      <animated.div style={fadeIn}>
+        <Stack>
+          <Paper elevation={10}>
+            <Stack
+              sx={{
+                height: "auto",
+                width: "600px",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                variant="h4s"
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "4rem",
+                  marginBottom: 30,
+                }}
+              >
+                Login
+              </Typography>
+
+              <Button
+                onClick={handleGoogleLogin}
+                startIcon={<GoogleIcon />}
+                variant="contained"
+                disableElevation
+                color="secondary"
+                sx={{
+                  backgroundColor: "#ff0000",
+                  fontWeight: "bold",
+                  height: "60px",
+                  width: "300px",
+                  margin: 2,
+                  transition: "all 0.5s ease",
+                }}
+              >
+                Login with Google
+              </Button>
+              <Button
+                onClick={handleGitHubLogin}
+                startIcon={<GitHubIcon />}
+                variant="contained"
+                disableElevation
+                color="secondary"
+                sx={{
+                  backgroundColor: "#000000",
+                  fontWeight: "bold",
+                  width: "300px",
+                  height: "60px",
+                  margin: 2,
+                  transition: "all 0.5s ease",
+                }}
+              >
+                Login with GitHub
+              </Button>
+            </Stack>
+          </Paper>
+        </Stack>
+      </animated.div>
+    </Grid>
   );
 }
